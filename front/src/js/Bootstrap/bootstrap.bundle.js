4546aa9d244b58e1461bac51046464b833d11044
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2025-03-22 19:07:05 +0200

    Home Page

--- front/src/js/Bootstrap/bootstrap.bundle.js
+++ front/src/js/Bootstrap/bootstrap.bundle.js
@@ -59,7 +59,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap util/index.js
+   * Bootstrap util/main.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
--- front/src/js/Bootstrap/bootstrap.js
+++ front/src/js/Bootstrap/bootstrap.js
@@ -78,7 +78,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap util/index.js
+   * Bootstrap util/main.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
